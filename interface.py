885f9a38c6dc39a3b911d064f5361bb20942513b
Nicholas Harley <29707986+n-harley@users.noreply.github.com>
2021-05-18 15:04:50 +0100

    Add files via upload

---
+++ interface.py
@@ -0,0 +1,25 @@
+import json
+
+with open('data.json') as f:
+  data = json.load(f)
+
+def lookup(x,s):
+    return data
+
+def domain(s):
+    return 1
+
+def get_pitch(o):
+    return 1
+
+def get_onset(o):
+    return 1
+
+def get_duration(o):
+    return 1
+
+def get_particles(o):
+    return 1
+
+def get(a):
+    return lambda o : return 1 
